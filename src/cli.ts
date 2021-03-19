0c23c3d497556464bc5c0ff287aaf0de1b17ade0
Shachar Itzhaky <corwin.amber@gmail.com>
2021-03-19 16:51:21 +0200

    [feature] [cli] Flag for out dir.

--- src/cli.ts
+++ src/cli.ts
@@ -1,18 +1,22 @@
 import commander from 'commander';
+import type { ProjectDefinition } from './project';
 import _ from './plug';
 
 function command(argv: string[]) {
     var o = commander
         .description('Kremlin bundler command-line interface')
         .usage('[options] <entry-points>')
-        .option('-w, --watch')
-        .option('-p, --prod')
-        .option('--node')
+        .option('-o, --out-dir <dir>', "output directory [build/kremlin]")
+        .option('-w, --watch', "watch for file changes")
+        .option('-p, --prod', "production mode (concatenate JS)")
+        .option('--node', "target Node.js environment " + 
+                          "(do not shim builtin modules)")
         .parse(argv);
 
     if (o.args.length == 0) return o.outputHelp();
 
-    var proj = {main: o.args};
+    var proj: ProjectDefinition = {main: o.args};
+    if (o.outDir) proj.buildDir = o.outDir;
     _.opts.mode = o.prod ? 'prod' : 'dev';
     _.opts.target = o.node ? 'node' : 'browser';
 
