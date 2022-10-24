d4ca8dc55b6f5444c6644a14ddf65b45bcbefac5
Shachar Itzhaky <corwin.amber@gmail.com>
2022-10-24 18:53:05 +0300

    [bugfix] Hack for SASS in NWjs.

--- src/build/addons/addon-sass.ts
+++ src/build/addons/addon-sass.ts
@@ -3,6 +3,18 @@ import sass from 'sass';
 import { ModuleRef, TransientCode } from '../modules';
 import { Transpiler } from '../transpile';
 
+/**
+ * @oops this is terrible! But the Dart compiler emits code that requires
+ * special treatment for Electron:
+ * https://github.com/mbullington/node_preamble.dart/blob/3c83ba0887fb64424d3336338da393e4f0eecbf4/lib/preamble.js#L50
+ * and this treatment is not applied to NWjs (probably because no one tried it).
+ * 
+ * The SASS compiler is written in Dart, so this code ends up in the lib.
+ * This messes up the Kremlin plug when running in NWjs.
+ */
+if (typeof process !== 'undefined' && process.versions['nw'])
+    process.versions['electron'] = 'FAKE (hack for sass@1.52.3:sass.dart.js)';
+
 
 class SASSCompiler implements Transpiler {
     match(filename: string) { return !!filename.match(/[.]s[ac]ss$/); }
