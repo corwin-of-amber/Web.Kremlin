185e088b3189751f9bdb272983d2cc8fca31f5ab
Shachar Itzhaky <corwin.amber@gmail.com>
2023-01-06 15:42:02 +0200

    [bugfix] SASS NWjs import order issue.

--- src/build/addons/addon-sass.ts
+++ src/build/addons/addon-sass.ts
@@ -1,10 +1,5 @@
-import sass from 'sass';
-
-import { ModuleRef, TransientCode } from '../modules';
-import { Transpiler } from '../transpile';
-
 /**
- * @oops this is terrible! But the Dart compiler emits code that requires
+ * @oops this is horrible! But the Dart compiler emits code that requires
  * special treatment for Electron:
  * https://github.com/mbullington/node_preamble.dart/blob/3c83ba0887fb64424d3336338da393e4f0eecbf4/lib/preamble.js#L50
  * and this treatment is not applied to NWjs (probably because no one tried it).
@@ -14,6 +9,12 @@ import { Transpiler } from '../transpile';
  */
 if (typeof process !== 'undefined' && process.versions['nw'])
     process.versions['electron'] = 'FAKE (hack for sass@1.52.3:sass.dart.js)';
+
+
+import sass from 'sass';
+
+import { ModuleRef, TransientCode } from '../modules';
+import { Transpiler } from '../transpile';
 
 
 class SASSCompiler implements Transpiler {
