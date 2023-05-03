b08aac731da92158da0929835afb0171239d36b8
Shachar Itzhaky <corwin.amber@gmail.com>
2023-05-03 17:56:44 +0300

    [qa] [refactor] Added Vue class test.
    Based on vue-facing-decorator.
    Vue tests moved to addons subdir.

---
+++ tests/addons/vue/class-dec.vue
@@ -0,0 +1,17 @@
+<template>
+    <div></div>
+  </template>
+  
+  <script lang="ts">
+  // @ts-ignore
+  import { Component, Vue } from 'vue-facing-decorator';
+
+  @Component
+  export default class E extends Vue {
+  
+  }
+  </script>
+  
+  <style>
+  div { background: black; }
+  </style>
--- tests/addons/vue/index.ts
+++ tests/addons/vue/index.ts
@@ -1,7 +1,10 @@
 // @ts-ignore
 import Div from './div.vue';
+// @ts-ignore
+import Dec from './class-dec.vue';
 
 
 function main() {
     let app = new Div();
+    app = new Dec();
 }
